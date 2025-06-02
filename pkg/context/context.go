dbf799ced43bc9664d88fabd202b4225f5a0fc3d
dtomschitz <david@tomschitz.de>
2025-06-02 10:19:03 +0200

    add helper functions for passing and retrieving values from the context

---
+++ pkg/context/context.go
@@ -0,0 +1,23 @@
+package context
+
+import "context"
+
+type (
+	Key string
+)
+
+const (
+	ClientVersion Key = "client_version"
+)
+
+func GetStringValue(ctx context.Context, key Key) string {
+	value := ctx.Value(key)
+	if value == "" {
+		return ""
+	}
+	if strValue, ok := value.(string); ok {
+		return strValue
+	}
+
+	return ""
+}
---
+++ pkg/context/context_test.go
@@ -0,0 +1,34 @@
+package context_test
+
+import (
+	"context"
+	commonCtx "github.com/dtomschitz/headless-go-client/pkg/context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_GetStringValue(t *testing.T) {
+	t.Run("WithValidKey", func(t *testing.T) {
+		ctx := context.WithValue(context.Background(), commonCtx.ClientVersion, "1.0.0")
+		value := commonCtx.GetStringValue(ctx, commonCtx.ClientVersion)
+		assert.Equal(t, "1.0.0", value, "expected value to match the context value")
+	})
+
+	t.Run("WithInvalidKey", func(t *testing.T) {
+		ctx := context.WithValue(context.Background(), commonCtx.ClientVersion, "1.0.0")
+		value := commonCtx.GetStringValue(ctx, "invalid_key")
+		assert.Equal(t, "", value, "expected empty string for invalid key")
+	})
+
+	t.Run("WithEmptyContext", func(t *testing.T) {
+		ctx := context.Background()
+		value := commonCtx.GetStringValue(ctx, commonCtx.ClientVersion)
+		assert.Equal(t, "", value, "expected empty string for empty context")
+	})
+
+	t.Run("WithNonStringValue", func(t *testing.T) {
+		ctx := context.WithValue(context.Background(), commonCtx.ClientVersion, 12345)
+		value := commonCtx.GetStringValue(ctx, commonCtx.ClientVersion)
+		assert.Equal(t, "", value, "expected empty string for non-string value")
+	})
+}
