029874321292e5fd8a4e772e3ae5f7565f3ed282
dtomschitz <david@tomschitz.de>
2025-06-04 22:34:54 +0200

    Move initial poll delay logic into goroutine in start method

--- config/service.go
+++ config/service.go
@@ -68,18 +68,19 @@ func NewConfigService(ctx context.Context, url string, opts ...ConfigServiceOpti
 }
 
 func (cs *ConfigService) start(ctx context.Context) error {
-	if cs.initialPollDelay > 0 {
-		cs.logger.Info("waiting for initial poll delay of %s before starting ConfigService", cs.initialPollDelay)
-		select {
-		case <-ctx.Done():
-			cs.logger.Warn("ConfigService stopped because context was cancelled")
-			return nil
-		case <-time.After(cs.initialPollDelay):
-			cs.logger.Info("initial poll delay completed, starting ConfigService")
-		}
-	}
 
 	go func() {
+		if cs.initialPollDelay > 0 {
+			cs.logger.Info("waiting for initial poll delay of %s before starting ConfigService", cs.initialPollDelay)
+			select {
+			case <-ctx.Done():
+				cs.logger.Warn("ConfigService stopped because context was cancelled")
+				return
+			case <-time.After(cs.initialPollDelay):
+				cs.logger.Info("initial poll delay completed, starting ConfigService")
+			}
+		}
+
 		ticker := time.NewTicker(cs.pollInterval)
 		defer ticker.Stop()
 
